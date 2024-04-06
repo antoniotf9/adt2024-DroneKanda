2ab6210e4860c89aba8574be86c89d88631a7448
Antonio Ferguson <antoniotf9@yahoo.co>
2024-04-06 03:10:13 -0400

    Add files via upload
    Initial commit done by Coach Antonio for DroneKanda Team #1 (Python code for the aerial drone autonomous challenge)

---
+++ AutotomasDrone/AutonomousDrone/Matlanding/Blue1.py
@@ -0,0 +1,3 @@
+class drone3m:
+    def scenario3(self):
+        print("This is the code for senario 3-M with Blue takeoff and Red landing")
---
+++ AutotomasDrone/AutonomousDrone/Matlanding/Red1.py
@@ -0,0 +1,3 @@
+class drone1m:
+    def scenario1(self):
+        print("This is the code for senario 1-M with red takeoff and blue landing")
